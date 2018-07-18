f2d22b2b2122fb71d6a8ed370b3f536343dba038
Hayden Singleton <hgs@vt.edu>
2018-07-18 18:01:51 +0800

    Working version, creates 2 col table

--- genCountryTable.py
+++ genCountryTable.py
@@ -1,16 +1,32 @@
-import sys
 import os
-from datetime import datetime
+# from datetime import datetime
 import pandas as pd
-from panadas import DataFrame
-import requests
-from bs4 import BeautifulSoup
+from pandas import DataFrame
 
 url = "https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements"
 
-response = requests.get(url)
-source_code = response.text
-soup = BeautifulSoup(source_code, "lxml")
+try:
+    os.mkdir("data")
+except Exception:  # Generic OS Error
+    pass
+os.chdir("data")
 
-df = pd.DataFrame() 
+# Get country data/list from the third table on the page
+df = pd.read_html(url)[2]
 
+# Fix column titles
+df.columns = list(df.iloc[0,:])
+df.drop(df.index[2])
+df.drop(df.index[0], inplace=True) # df = df[1:]
+
+# Delete extra columns
+df.drop(df.columns[2:], axis=1, inplace=True)
+
+df.set_index('Code', inplace=True)
+
+filename = "countries.csv"
+filepath = os.path.join(os.getcwd(), filename)
+df.to_csv(path_or_buf=filepath)
+
+
+print(df.head())
